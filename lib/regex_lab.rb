72731bc5629b28645024d66fc5fb5dad8fe02f53
Stephen Olson <stephenolson@gmail.com>
2018-09-18 19:23:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,11 @@ require 'pry'
 def starts_with_a_vowel?(word)
 # (/[aeiou]/)
 
-  word.match(/\A[aeiou]/i)
+  word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
