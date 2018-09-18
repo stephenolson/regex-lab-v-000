aa5842ec1f6f86214c790655927a0a51da324d43
Stephen Olson <stephenolson@gmail.com>
2018-09-18 19:00:17 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,19 @@
 def starts_with_a_vowel?(word)
-  
+  word.
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.
 end
 
 def words_five_letters_long(text)
+  text.
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.
 end
 
 def valid_phone_number?(phone)
+  phone.
 end
