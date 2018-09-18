b9116f716462ede1c27afa88805e7364eb60b94c
Stephen Olson <stephenolson@gmail.com>
2018-09-18 19:01:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,17 @@
 def starts_with_a_vowel?(word)
-  word.
+  word.scan(/(\d+)-(\d+)-(\d+)/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.
+  text.scan(/(\d+)-(\d+)-(\d+)/)
 end
 
 def words_five_letters_long(text)
-  text.
+  text.scan(/(\d+)-(\d+)-(\d+)/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.
+  text.scan(/(\d+)-(\d+)-(\d+)/)
 end
 
 def valid_phone_number?(phone)
