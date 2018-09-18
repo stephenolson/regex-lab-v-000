1b2bb23cde6ba2235ee5f740b0ad0c82dc674e17
Stephen Olson <stephenolson@gmail.com>
2018-09-18 19:28:42 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,11 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].*\.$/) == nil
-    return false
-  else
-    return true
-  end
+  !!text.match(/^[A-Z].*\.$/)
 end
 
 def valid_phone_number?(phone)
