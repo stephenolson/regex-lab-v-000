d3ca9d9b7eaa92b0a247c1ce0d3c5a7a7826e338
Stephen Olson <stephenolson@gmail.com>
2018-09-18 19:22:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,13 @@
+require 'pry'
+
 def starts_with_a_vowel?(word)
-  word.match(/\b[aeiou]/)
+# (/[aeiou]/)
+
+  if word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,9 +19,17 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/(\d+)-(\d+)-(\d+)/)
+  if text.match(/^[A-Z].*\.$/) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/(\d+)-(\d+)-(\d+)/)
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+    return false
+  else
+    return true
+  end
 end
